12899e64a792adb0249805fa45b5efb012fdece7
Furkan KAPLAN <furkankaplan@outlook.com>
2020-08-03 16:40:28 +0300

    Update example project with library implementation

--- app/src/main/java/com/github/furkankaplan/blurviewexample/MainActivity.java
+++ app/src/main/java/com/github/furkankaplan/blurviewexample/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.github.furkankaplan.blurview.BlurView;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BlurView blurView = findViewById(R.id.fkBlurView);
+        blurView.getBlur(this, blurView);
     }
 }
